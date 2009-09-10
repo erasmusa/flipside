7243d815136da13999821d7369cd5c89fde829e2
Nicholas Flynt <thegamefreak0134@zoho.com>
2009-09-09 23:36:26 -0500

    More complete animation implementation, it's starting to take shape. Multiple frames, image offset, frame delay, all neatly packaged in an Animation class. Yay!

--- actor.py
+++ actor.py
@@ -19,12 +19,41 @@ class Actor:
 		def __init__(self, frames):
 			self.frame = list()
 			for frame in frames:
-				self.frame.append(Frame(frame['src'], frame['length'] if 'length' in frame else 1))
+				billy = Frame(frame['src'], frame.get('length', 1))
+				billy.rect = billy.rect.move( frame.get('x-offset', 0), frame.get('y-offset', 0) )
+				self.frame.append(billy)
 	def __init__(self, filename):
 		with open(filename) as actor: 
 			self.base = json.load(actor)
 		for state in self.base['states']:
 			self.state[state['name']] = Actor.AnimState(state['frames'])
+			
+class Animation:
+	# This class is used by game objects after actors have been loaded. It expects an Actor instance,
+	# and will maintain the animation state for that actor by exposing some very handy functions.
+	def __init__(self, Actor, State):
+		self.actor = Actor
+		self.state = State
+		self.frame = 0
+		self.framepos = 0
+	def draw(self, screen, x, y):
+		#Pick out the current image, based on the state and frame data
+		image = self.actor.state[self.state].frame[self.frame].image
+		rect  = self.actor.state[self.state].frame[self.frame].rect
+		draw_pos = rect.move(x, y)
+		screen.blit(image, draw_pos)
+	def update(self):
+		#This function advances the frame counter, accounting for any delays.
+		self.framepos += 1
+		if (self.framepos >= self.actor.state[self.state].frame[self.frame].length):
+			self.frame += 1
+			self.framepos = 0
+			if self.frame >= len(self.actor.state[self.state].frame):
+				self.frame = 0
+	def setState(self, newstate):
+		self.state = newstate
+		self.frame = 0
+		self.framepos = 0
 
 
 class Player(Actor):
@@ -34,8 +63,8 @@ class Player(Actor):
 	speed = [0, 0]
 	def __init__(self, filename):
 		Actor.__init__(self, filename)
-		self.frame = 0;
-		self.state = 'stand'
+		self.anim = Animation(Actor, 'stand')
+		
 		
 	def draw(self, screen):
 		#physics
@@ -43,14 +72,8 @@ class Player(Actor):
 		self.y += self.speed[1]
 		
 		#draw	
-		self.image = Actor.state[self.state].frame[self.frame].image
-		self.rect  = Actor.state[self.state].frame[self.frame].rect
-		self.frame += 1
-		if self.frame >= len(Actor.state[self.state].frame):
-			self.frame = 0
-		
-		draw_pos = self.rect.move(self.x, self.y)
-		screen.blit(self.image, draw_pos)
+		self.anim.draw(screen, self.x, self.y)
+		self.anim.update()
 		
 	def handleKey(self, event):
 		if event.type == pygame.KEYDOWN:
@@ -60,12 +83,11 @@ class Player(Actor):
 				self.speed[1] = -1
 			if event.key == pygame.K_RIGHT:
 				self.speed[0] = 1
-				self.state = 'walk'
-				self.frame = 0
+				self.anim.setState('walk')
 			if event.key == pygame.K_LEFT:
 				self.speed[0] = -1
 		if event.type == pygame.KEYUP:
-			self.state = 'stand'
+			self.anim.setState('stand')
 			self.frame = 0
 			if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
 				self.speed[1] = 0
